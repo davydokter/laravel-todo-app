8041fe0318ca99c49594db6070d2929579f6eacc
Davy Dokter <dxvy.dokter@gmail.com>
2023-12-19 20:35:45 +0100

    Updated request validations

--- app/Http/Requests/UpdateToDoRequest.php
+++ app/Http/Requests/UpdateToDoRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreToDoRequest extends FormRequest
+class UpdateToDoRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,8 @@ class StoreToDoRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => ['required', 'string', 'max:255'],
-            'description' => ['required', 'string', 'max:255'],
-            'due_date' => ['required', 'date'],
+            'todo-id' => ['required', 'int', 'max:255'],
+            'completed' => ['string']
         ];
     }
 }
